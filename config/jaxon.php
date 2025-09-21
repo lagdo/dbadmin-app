541e6381db795065036e3ea1198d6e0ec3391975
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-09-21 09:12:11 +0200

    Fix missing middleware in the Jaxon request route.

--- config/jaxon.php
+++ config/jaxon.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\DbAdminLogWriter;
 use Lagdo\DbAdmin\Config\UserFileReader;
 
 return [
@@ -9,6 +10,7 @@ return [
             'middlewares' => [
                 'web', // Includes the Illuminate\Session\Middleware\StartSession
                 // middleware, which returns a 419 error when the sessions has expired.
+                DbAdminLogWriter::class,
             ],
         ],
         'directories' => [],
